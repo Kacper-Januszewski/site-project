32ae22200bffd1d979132c287bd1a898348d1df5
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-10-15 22:40:08 +0200

    fixed sidebar compile issue

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Link from "next/link";
-import Logo from "./logo";
-import Button from "./button";
 
 
 const Navbar = () => {
@@ -10,7 +8,6 @@ const Navbar = () => {
             <div className="w-full h-20 bg-gray-700 bg-opacity-25 sticky top-0">
                 <div className="container mx-auto px-4 h-full">
                     <div className="flex justify-between items-center h-full">
-                        <Logo />
                         <ul className="hidden md:flex gap-x-6 text-white">
                             <li>
                                 <Link href="https://github.com/Kacper-Januszewski" passHref>
@@ -23,7 +20,6 @@ const Navbar = () => {
                                 </Link>
                             </li>
                         </ul>
-                        <Button />
                     </div>
                 </div>
             </div>
