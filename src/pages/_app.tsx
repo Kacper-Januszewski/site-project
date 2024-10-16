726b79bffa8908b1525a488a5cada5c3bfa20e76
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-10-16 15:00:53 +0200

    issue check

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import '@fontsource/inter/variable.css';
-import '../styles/globals.css';
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
+// import '@fontsource/inter/variable.css';
+// import '../styles/globals.css';
+//
+// function MyApp({ Component, pageProps }) {
+//     return <Component {...pageProps} />;
+// }
+//
+// export default MyApp;
