f0da2f3d0930bf056dbcd640d76d75156adb1fc7
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-10-16 11:22:52 +0200

    increasing a navbar gap

--- src/app/components/navigation/navbar/index.tsx
+++ src/app/components/navigation/navbar/index.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
             <div className="w-full h-20 bg-black bg-opacity-25 backdrop-blur-[5px] sticky top-0">
                 <div className="container mx-auto px-4 h-full">
                     <div className="flex justify-center items-center h-full">
-                        <ul className="hidden md:flex gap-x-6 text-white">
+                        <ul className="hidden md:flex gap-x-30 text-white">
                             <li>
                                 <Link href="https://github.com/Kacper-Januszewski" passHref>
                                     <a>GitHub</a>
