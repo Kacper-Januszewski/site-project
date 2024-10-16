659ecfa269d80f6af5285788cc4a2dc7e5b0ff0c
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-10-16 18:24:40 +0200

    cta form

--- src/app/components/cta/form/index.tsx
+++ src/app/components/cta/form/index.tsx
@@ -10,7 +10,7 @@ const Form = () => {
                 <p className="pb-6">
                     You scrolled this far? Contact me!
                 </p>
-                <div className="container flex justify-center gap-x-16 h-10">
+                <div className="container flex justify-center gap-x-16 h-10 mb-6">
                     <Link href="https://github.com/Kacper-Januszewski">
                     <Image
                         src="/github-mark-white.svg"
@@ -31,6 +31,15 @@ const Form = () => {
                         className="object-contain"
                     />
                     </Link>
+                </div>
+                <div className='flex justify-center items-center p-8'>
+                    <form className='flex flex-col w-fit'>
+                        <input className='rounded p-2 mb-4 border border-white bg-black' id='email' type='email' aria-label='email address' placeholder='E-mail' />
+                        <input className="h-20 rounded p-2 mb-4 border border-white bg-black" id="message" type="text" aria-label="message" placeholder="Message"></input>
+                        <button className='bg-white hover:bg-gray-500 duration-300 text-black shadow p-2 rounded' type='submit'>
+                            Send
+                        </button>
+                    </form>
                 </div>
                 <div className="h-[500px]"></div>
             </div>
