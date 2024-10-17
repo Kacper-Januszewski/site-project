16df59aa18642067bea11b8eb0d109bfae8e78f7
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-10-17 20:48:37 +0200

    form email sender

--- src/app/components/cta/form/index.tsx
+++ src/app/components/cta/form/index.tsx
@@ -1,9 +1,25 @@
+'use client';
+
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { sendEmail } from "@/utils/email-send";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
+export type FormData = {
+    name: string;
+    email: string;
+    message: string;
+};
 
-const Form = () => {
+const Form: FC = () => {
+    const {register, handleSubmit } = useForm<FormData>();
+
+    function onSubmit(data: FormData) {
+        sendEmail(data)
+    }
+
     return (
         <>
             <div>
@@ -22,21 +38,42 @@ const Form = () => {
                     />
                     </Link>
                     <Link href="https://www.linkedin.com/in/kacper-januszewski/">
-                    <Image
+                    <img
                         src="/LI-In-Bug.png"
                         alt="LinkedIn Logo"
                         width={50}
                         height={40}
-                        priority
                         className="object-contain"
                     />
                     </Link>
                 </div>
                 <div className='flex justify-center items-center p-8'>
-                    <form className='flex flex-col w-fit'>
-                        <input className='rounded p-2 mb-4 border border-white bg-black' id='email' type='email' aria-label='email address' placeholder='E-mail' />
-                        <input className="h-20 rounded p-2 mb-4 border border-white bg-black" id="message" type="text" aria-label="message" placeholder="Message"></input>
-                        <button className='bg-white hover:bg-gray-500 duration-300 text-black shadow p-2 rounded' type='submit'>
+                    <form
+                        className='flex flex-col w-fit'
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        <input
+                            className='rounded p-2 mb-4 border border-white bg-black'
+                            id='email'
+                            type='email'
+                            aria-label='email address'
+                            placeholder='E-mail'
+                            {...register('email', { required: true})}
+                        />
+
+                        <textarea
+                            rows={4}
+                            className="h-20 rounded p-2 mb-4 border border-white bg-black"
+                            id="message"
+                            type="text"
+                            aria-label="message"
+                            placeholder="Message"
+                            {...register('message', {required: true})}
+                        ></textarea>
+
+                        <button
+                            className='bg-white hover:bg-gray-500 duration-300 text-black shadow p-2 rounded'
+                            type='submit'>
                             Send
                         </button>
                     </form>
