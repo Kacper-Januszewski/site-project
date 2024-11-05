285d0e776d0bae17368aeb05a95b8b3616dc85af
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-11-05 13:25:38 +0100

    preloading but it doesn't really work

--- src/app/components/content/carouseled_content/carousel/index.tsx
+++ src/app/components/content/carouseled_content/carousel/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import React, { useEffect, useState} from "react";
+import Image from 'react';
 
 interface CarouselItem {
     id: number;
@@ -15,6 +16,15 @@ interface DynamicJSONInput {
 const Carousel: React.FC<DynamicJSONInput> = ({filename}) => {
     const [items, setItems] = useState<CarouselItem[]>([]);
     const [index, setIndex] = useState(0);
+    const [preloadedImages, setPreloadedImages] = useState<{
+        current: CarouselItem | null;
+        next: CarouselItem | null;
+        previous: CarouselItem | null;
+    }>({
+        current: null,
+        next: null,
+        previous: null
+    });
 
     const handleNext = () => {
         setIndex((prevIndex) => (prevIndex + 1) % items.length);
@@ -29,16 +39,23 @@ const Carousel: React.FC<DynamicJSONInput> = ({filename}) => {
             const response = await fetch(`/${filename}`);
             const data = await response.json();
             setItems(data.items.bootstrap);
-
         };
 
         fetchItems();
     }, [filename]);
 
+    useEffect(() => {
+        setPreloadedImages((prevState) => ({
+            previous: items[(index - 1 + items.length) % items.length] || null,
+            current: items[index] || null,
+            next: items[(index + 1) % items.length] || null,
+        }));
+    }, [index,items]);
+
     return (
         <div className="relative w-full max-w-md min-w-[384px] md:min-w-[450px] h-fit">
             <div className="overflow-hidden rounded-lg">
-                <img src={items[index]?.imageUrl} alt="slides" className="w-full h-64 object-cover" />
+                <img src={preloadedImages.current?.imageUrl} alt="slides" className="w-full h-64 object-cover" />
                 <div className="p-4 bg-gray-900 shadow-lg">
                     <h3 className="text-xl font-semibold">{items[index]?.title}</h3>
                     <p className="text-gray-600">{items[index]?.body}</p>
