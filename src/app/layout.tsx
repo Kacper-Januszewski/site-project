7d901bfdbaf359a8a0959da92f11a33f48db00c7
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-11-20 14:11:02 +0100

    setup new branch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Navigation from "./components/navigation/navbar"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -18,7 +17,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-      <Navigation />
       {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,53 @@
-import Cta from "@/app/components/cta/form";
-import Carousel from "@/app/components/content/carouseled_content/carousel";
 
 export default function Home() {
   return (
-      <main className="h-full text-white">
-        <div className="container mx-auto px-4 font-sans">
-            <section id="home" className="pt-[100px]">
-              <h1 className="text-xl text-center leading-[50px]">
-                  Hello! My name is <br /><span className="font-bold text-5xl">Kacper Januszewski</span> <br />and I&apos;m a <br /><span className="font-medium text-4xl">Web Developer</span>
-              </h1>
-            </section>
-            <div className="pt-[200px] pb-[200px]">
-                <div id="work" className="flex flex-wrap h-fit bg-black pt-[400px] gap-x-36 justify-center">
-                    <div className="order-1">
-                        <Carousel filename={'CodeItems.json'}/>
-                    </div>
-                    <div className="order-2">
-                        <p className='text-center w-auto max-w-[448px] mx-auto '>My work consists of multiple RESTful API projects in .NET as well as using code-first approach. I have also set-up a simple multi-service web app using docker</p>
-                    </div>
-                </div>
-                <div className="flex flex-wrap h-fit pt-[400px] gap-x-36 justify-center pb-[400px]">
-                    <div className="order-2 md:order-1">
-                        <p className='text-center w-auto max-w-[448px] mx-auto'>Aesthetic experience is something that I take very seriously. I have finished a filmmaking highschool where I learned photography and design, both of which are proving useful even after I changed my industry career path.</p>
-                    </div>
-                    <div className="order-1 md:order2">
-                        <Carousel filename={'PhotoItems.json'}/>
-                    </div>
-                </div>
-            </div>
-                <section id="contact" className="text-center text-xl">
-                    <Cta />
-                </section>
-        </div>
+      <main>
+          <div>
+              Kacper Januszewski <br /> Web developer
+          </div>
+          <div>
+              <div>
+                  My work
+                  <div>
+                      Github <br />
+                      Live sites <br />
+                      Aesthetic <br />
+                  </div>
+              </div>
+              <div>
+                  Contact me
+                  <div>
+                      LinkedIn <br />
+                      Github <br />
+                      Email <br />
+                  </div>
+              </div>
+          </div>
+          <div>
+              <div>
+                  <div>
+                      © 2024 Kacper Januszewski
+                  </div>
+                  <div>
+                      <div>
+                          My work
+                      </div>
+                      <div>
+                          logo <br/>
+                          logo
+                      </div>
+                  </div>
+                  <div>
+                      <div>
+                          Get in touch!
+                      </div>
+                      <div>
+                          logo <br/>
+                          logo
+                      </div>
+                  </div>
+              </div>
+          </div>
       </main>
   );
 }
