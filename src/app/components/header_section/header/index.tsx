0adc2df963076e0ffbe5af899f10f7f7472be17b
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-11-21 17:32:40 +0100

    header test

--- src/app/components/header_section/header/index.tsx
+++ src/app/components/header_section/header/index.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <>
-            <div className="text-2xl font-medium leading-4 p-2 m-2 md:text-6xl md:font-semibold md:leading-10 md:px-10 md:mx-10">
+            {/* deleted but idk p-2 m-2*/}
+            <div className="text-2xl font-medium leading-4 md:text-6xl md:font-semibold md:leading-10 md:px-10 md:mx-10">
                 Kacper Januszewski<br /><span className="text-sm font-extralight md:text-4xl md:font-extralight"> Web Developer  </span>
             </div>
         </>
