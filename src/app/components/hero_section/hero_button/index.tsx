89aa03ee2d5d0dca624b4bec55552e934f3aee7c
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-11-21 21:08:10 +0100

    dynamic buttons

--- src/app/components/hero_section/hero_button/index.tsx
+++ src/app/components/hero_section/hero_button/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import {Button} from "react-bootstrap";
 
-const HeroButton = () => {
+interface ButtonData {
+    id: number;
+    title: string;
+    description: string;
+    url: string;
+}
+
+const HeroButton: React.FC<DynamicJSONInput> = () => {
     return (
         <>
                 <Button className="bg-button-gray rounded m-0.5 px-12 py-1 md:m-1 md:px-20 md:py-1 md:text-2xl">Button</Button>
