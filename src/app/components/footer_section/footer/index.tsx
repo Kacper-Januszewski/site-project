d26d75ff6bc5fdcd6739c6ef3a8f026f20f50c58
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-11-21 17:18:35 +0100

    footer styling

--- src/app/components/footer_section/footer/index.tsx
+++ src/app/components/footer_section/footer/index.tsx
@@ -4,15 +4,15 @@ import Image from "next/image";
 const Footer = () => {
     return (
         <>
-            <div className="h-[1px] w-full md:h-[1px] md:w-full bg-button-gray" />
+            <div className="h-[0.5px] w-full md:h-[1px] md:w-full bg-button-gray" />
             <div className="flex flex-col items-center md:flex md:flex-row md:justify-between md:items-center md:mt-8">
-                    <p className="text-xs order-2 text-copyright-gray md:order-1 md:text-xs md:mr-[40px]">© 2024 Kacper Januszewski</p>
+                    <p className="p-1 m-1 text-xs order-2 text-copyright-gray md:order-1 md:text-xs md:mr-[40px]">© 2024 Kacper Januszewski</p>
                 <div className="order-1 md:order-2 md:flex md:flex-row md:gap-x-10">
-                    <div className="md:flex md:flex-col md:gap-y-3 md:items-center">
+                    <div className="p-2 m-2 flex flex-col items-center md:flex md:flex-col md:gap-y-3 md:items-center">
                         <div>
                             My work
                         </div>
-                        <div className="flex flex-row gap-x-4 md:gap-x-7">
+                        <div className="p-1 m-1 flex flex-row gap-x-10 md:gap-x-7">
                             <a href="https://github.com/Kacper-Januszewski">
                                 <Image src='/icons/github-mark.svg' alt="github icon" width="20" height="20"/>
                             </a>
@@ -21,11 +21,11 @@ const Footer = () => {
                             </a>
                             </div>
                     </div>
-                    <div className="md:flex md:flex-col md:gap-y-3 md:items-center">
+                    <div className="p-2 m-2 flex flex-col items-center md:flex-col md:gap-y-3 md:items-center">
                         <div>
                             Get in touch!
                         </div>
-                        <div className="flex flex-row gap-x-4 md:gap-x-7">
+                        <div className="p-1 m-1 flex flex-row gap-x-10 md:gap-x-7">
                             <a href="mailto:januszewskidev@gmail.com">
                                 <Image src='/icons/unnamed.png' alt="gmail icon" width="20" height="20"/>
                             </a>
