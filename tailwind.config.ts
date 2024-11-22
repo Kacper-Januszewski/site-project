22d31851fdade73976740250434a0023a55fd533
Kacper Januszewski <kacper.januszewski@gmail.com>
2024-11-23 00:40:43 +0100

    material tailwind configuration failed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,13 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-const withMT = require("@material-tailwind/html/utils/withMT");
+const withMT = require("@material-tailwind/react/utils/withMT");
 
 module.exports = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
