3e0ef441d2b58a1b301ea118549ebfa6a45918c4
Kacper Januszewski <kacper.januszewski@gmail.com>
2025-03-03 22:33:13 +0100

    changed one github name to repositories, fixed github links, added temporary site to live sites

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import RowUC from "@/app/testing/row";
+
+const Projects = () => {
+    return (
+        <div className="flex justify-center items-center w-full min-h-screen text-7xl">
+                <p>Under Construction</p>
+        </div>
+    );
+};
+
+export default Projects;
